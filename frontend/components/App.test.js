552177a605974d654b25db9d39fa9b563a898c4d
abanoub nagib <abanoub.nagib@hotmail.com>
2023-08-25 15:15:07 -0700

    added couple more test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -12,10 +12,42 @@ test('renders without errors', () => {
  render(<AppFunctional />)
 });
 
-test("renders the header", () => {
+test("renders the leftButton", () => {
   render(<AppFunctional />)
 
   const leftButton = screen.queryByText(/LEFT/i);
+
+  expect(leftButton).toBeInTheDocument();
+});
+
+test("renders the rightButton", () => {
+  render(<AppFunctional />)
+
+  const leftButton = screen.queryByText(/right/i);
+
+  expect(leftButton).toBeInTheDocument();
+});
+
+test("renders the upButton", () => {
+  render(<AppFunctional />)
+
+  const leftButton = screen.queryByText(/up/i);
+
+  expect(leftButton).toBeInTheDocument();
+});
+
+test("renders the downButton", () => {
+  render(<AppFunctional />)
+
+  const leftButton = screen.queryByText(/down/i);
+
+  expect(leftButton).toBeInTheDocument();
+});
+
+test("renders the resetButton", () => {
+  render(<AppFunctional />)
+
+  const leftButton = screen.queryByText(/reset/i);
 
   expect(leftButton).toBeInTheDocument();
 });
